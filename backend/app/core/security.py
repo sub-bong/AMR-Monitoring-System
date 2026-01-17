ef5533238e595111122c427ca9a5d7c363044e15
sub-bong <kim5ys@naver.com>
2026-01-17 13:56:09 +0900

    docs: 회원가입, 로그인, 디바이스 인증 주석 추가
    원리 설명
    토큰 기능별 구분

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,4 +1,5 @@
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
+import pytz
 from typing import Any, Optional
 
 from jose import jwt
@@ -6,14 +7,27 @@ from passlib.context import CryptContext
 
 from app.core.config import settings
 
+seoul_tz = pytz.timezone("Asia/Seoul")  # 대한민국/서울 time zone
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+'''
+security.py 토큰 생성과 비밀번호 검증 기능
+'''
 
 
 def hash_password(password: str) -> str:
+    '''
+    ### password 해싱 기능(bcrypt)
+    '''
     return pwd_context.hash(password)
 
 
 def verify_password(plain: str, hashed: str) -> bool:
+    '''
+    ### password 해싱 검증
+    prams: plain, hashed
+    '''
     return pwd_context.verify(plain, hashed)
 
 
@@ -22,7 +36,21 @@ def create_access_token(
         subject_type: str,
         expires_min: Optional[int] = None,
 ) -> str:
-    now = datetime.now(timezone.utc)
+    '''
+    ### 토큰 생성 기능
+    ### 데이터 구조
+    sub: id
+    typ: user/device 구분
+    iat: 토큰 발급 시간
+    exp: 토큰 만료 시간
+    {
+      "sub": "123",
+      "typ": "user",
+      "iat": 1736851200,
+      "exp": 1736858400
+    }
+    '''
+    now = datetime.now(seoul_tz)
     exp = now + timedelta(minutes=expires_min or settings.jwt_expires_min)
     payload: dict[str, Any] = {
         "sub": subject,
